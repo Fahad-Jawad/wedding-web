c642cef019725b059f8a4605bdffc7fd8bf1470f
Fahad Jawad <fahadjawad596@gmail.com>
2025-01-22 21:31:59 +0500

    minor changings in wave and nav

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,6 +10,8 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary:'#e1bad0',
+        primaryLight:'#fce2f1'
       },
     },
   },
